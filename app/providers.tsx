0dbbdad7e0c92c610484df2a9d80c19947a73c55
mqteuss <mh.umateus@gmail.com>
2026-05-30 13:30:55 -0300

    Add app providers

---
+++ app/providers.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { ReactNode } from 'react';
+import { AuthProvider } from '@/components/AuthProvider';
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+}
